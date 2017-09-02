dbbba8f476d1d2555809241073b539e43fb430dd
Wyatt Sheffield <wyatt@pixil.xyz>
2017-09-02 16:56:21 -0500

    working flickering effect

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 all:
 	g++ -std=c++14 -g -lncurses main.cpp -o flames
+
+st:
+	g++ -std=c++14 -g -lncurses main.cpp -DSTEP -o flames
--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include <ctime>
 #include <unistd.h>
 #include <vector>
+#include <string>
 
-int SCW, SCH, maxtemp;
+int SCW, SCH, maxtemp, gen;
 
 int** init(int y, int x){
     int** out = new int*[y];
@@ -34,24 +35,40 @@ int weightedprob(int max){
     return r < 0 ? 0 : r;
 }
 
-inline float dist(float x, float y){
-    return (sqrt((x*x) + (y*y)) / 100) + 0.1;
+float dist(float x, float y){
+    float d = sqrt((x*x) + (y*y));
+    return d  == 0 ? 0.5 : d;
 }
 
-void nextframe(int** field, int** count){
-    for(int i = 1; i < SCH + 1; i++){
+float hotplate_temp_at(int* hotplate, int x){
+    float total = 0;
+    for(int i = -4; i <=4; i++){
+        int j = x + i;
+        if(j <0 || j >= SCW){
+            total +=0;
+        }
+        else{
+            total += hotplate[j];
+        }
+    }
+    return (total * maxtemp) / 9.0;
+}
+
+void nextframe(int** field, int** count, int* hotplate){
+    for(int i = 1; i <= SCH; i++){
         for(int j = 0; j < SCW; j++){
             float avg = 0;
             //int temp = rand() % maxtemp * 4;
             int counter = 0;
             for(int xoff = -7; xoff <= 7; xoff++){
-                for(int yoff = 1; yoff <= 7; yoff++){
+                for(int yoff = 1; yoff < 7; yoff++){
                     int y = i + yoff;
+                    y = y < 0 ? 0 : y;
                     int x = j + xoff;
                     if(x < 0)         avg += 0;
                     else if(x >= SCW) avg += 0;
-                    else if(y >= SCH)  avg += field[SCH][x] / dist(x, y);
-                    else avg += field[y][x] / dist(y, x);
+                    else if(y >= SCH)  avg += hotplate_temp_at(hotplate,x);
+                    else avg += field[y][x];
                     counter++;
                 }
             }
@@ -67,25 +84,50 @@ void nextframe(int** field, int** count){
     }
 }
 
-//sets the heating element at the bottom of the screen
-void hotplate(int** field){
-    int t;
+void wolfram(int* world, const int rule){
+    int l,c,r;
+    int* next = new int[SCW];
+    int current;
     for(int i = 0; i < SCW; i++){
-        t = maxtemp * (sin(float(i)/10.0) + 2) /3;
-        field[SCH][i] = t > maxtemp ? maxtemp : t;
+        l = i-1 < 0 ? 0 : world[i-1];
+        c = world[i];
+        r = i+1 >= SCW ? 0 : world[i+1];
+        current = (l<<2) | (c<<1) | r;
+        next[i] = ((1<<current) & rule) > 0 ? 1 : 0;
     }
+
+    for(int i = 0; i < SCW; i++){
+        world[i] = next[i];
+    }
+    delete[] next;
 }
+
 int time_in_ms(){
     return std::clock() / (double)(CLOCKS_PER_SEC / 1000);
 }
 
+//clamps the framerate, returns true only every n milliseconds
+bool advance(int n){
+#ifndef STEP
+    return (time_in_ms() % n) <= 10;
+#else
+    return true;
+#endif
+}
+
 void flames(){
-    int** field = init(SCH + 1, SCW);
+    int** field = init(SCH, SCW);
     int** count = init(SCH, SCW);
-    hotplate(field);
+    int* hotplate = new int[SCW];
+    
+    for(int i = 0; i < SCW; i++){
+        hotplate[i] = rand() % 2;
+    }
+    
     char c, disp;
     while((c = getch()) != 'q'){
-        if((time_in_ms() % 100 ) <= 10){
+        if(advance(100)){
+            gen++;
             for(int i = 0; i < SCH; i++){
                 for(int j = 0; j < SCW; j++){
                     move(i,j);
@@ -96,13 +138,14 @@ void flames(){
                     attroff(COLOR_PAIR(color));
                 }
             }
-            nextframe(field, count);
+            nextframe(field, count, hotplate);
+            wolfram(hotplate, 90);
+            mvaddstr(0,0, std::to_string(gen).c_str());
             refresh();
-            //sleep(1);
         }
     }
     refresh();
-    deallocate(field, SCH + 1);
+    deallocate(field, SCH);
     deallocate(count, SCH);
 }
 
@@ -127,11 +170,15 @@ void set_colors(){
 }
 
 int main(int argc, char** argv){
-    maxtemp = 64;
+    gen = 0;
+    maxtemp = 16;
     srand(time(NULL));
     initscr();
+    curs_set(0);
     cbreak();
+    #ifndef STEP
     timeout(0);
+    #endif
     noecho();
     keypad(stdscr, TRUE); 
     getmaxyx(stdscr, SCH, SCW);
